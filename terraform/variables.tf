d90e97097472c0dbb767b2358d4b39384d9d2b8c
uj-cpu-dev <134852095+uj-cpu-dev@users.noreply.github.com>
2024-12-25 01:49:52 +0000

    test commit 70

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "bucket" {
+  type = string
+  default= ""
+}
+
+variable "role_arn" {
+  type = string
+  default= ""
+}
+
+variable "region" {
+  type = string
+  default= ""
+}
+
+variable "key" {
+  type = string
+  default= ""
+}

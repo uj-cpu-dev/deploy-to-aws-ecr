362c91803188cad2c0f02f84d85f05fdf8d124e2
uj-cpu-dev <134852095+uj-cpu-dev@users.noreply.github.com>
2024-12-25 03:35:03 +0000

    test commit 77

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,18 @@
 data "aws_caller_identity" "current" {}
 
+# Check if the repository exists
+data "aws_ecr_repository" "existing" {
+  name = var.repository_name
+  # Suppress errors if the repository does not exist
+  lifecycle {
+    ignore_errors = true
+  }
+}
+
+# Create a new repository only if it does not exist
 resource "aws_ecr_repository" "repository" {
+  count = data.aws_ecr_repository.existing.id == "" ? 1 : 0
+
   name                 = var.repository_name
   image_tag_mutability = "IMMUTABLE"
   image_scanning_configuration {
